11b8761ebb44b283fa545c2c19dc562e9aee65fb
Garima Wadhwa <n01552997@humber.ca>
2024-04-08 13:18:32 -0400

    firs

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(
     cors(
         {
-            origin: '*' ,
+            origin: 'https://react-server-chi.vercel.app' ,
     allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept',
         }
     )
